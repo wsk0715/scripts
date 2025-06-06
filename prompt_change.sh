8547bf876100dc3355e020ff0112b16d3455872f
wsk0715 <leindey@gmail.com>
2025-06-06 15:03:58 +0900

    프롬프트 변경 스크립트 수정
    - 스타일 수정

--- prompt_change.sh
+++ prompt_change.sh
@@ -3,7 +3,7 @@
 set -e
 
 # [사용자명@호스트명] 현재경로$
-PROMPT='export PS1="\[\e[32m\][\u@\h]\[\e[0m\] \[\e[34m\]\W\[\e[33m\]\[\e[0m\]\$ "'
+PROMPT='export PS1="\[\e[32m\][\u@\h]\[\e[0m\] \[\e[33m\]\W\[\e[0m\]\[\e[37m\]\$\[\e[0m\] "'
 
 if ! grep -Fxq "$PROMPT" ~/.bashrc; then
   echo -e "\n$PROMPT" >> ~/.bashrc
